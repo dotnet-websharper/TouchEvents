548f8c7010b84e8dd6e65e3923274033644dd9da
Souvanxay LUANGSOUVANNAVONG <souvanxay38@gmail.com>
2024-10-08 09:50:08 +0200

    Initial commit; Add binding project

---
+++ WebSharper.TouchEvents/Main.fs
@@ -0,0 +1,106 @@
+﻿namespace WebSharper.TouchEvents
+
+open WebSharper
+open WebSharper.JavaScript
+open WebSharper.InterfaceGenerator
+
+module Definition =
+    let TouchInit =
+        Pattern.Config "TouchInit" {
+            Required = [
+                "identifier", T<int64>
+                "target", T<Dom.EventTarget>
+            ]
+            Optional = [
+                "clientX", T<float>
+                "clientY", T<float>
+                "screenX", T<float>
+                "screenY", T<float>
+                "pageX", T<float>
+                "pageY", T<float>
+                "radiusX", T<float>
+                "radiusY", T<float>
+                "rotationAngle", T<float>
+                "force", T<float>
+            ]
+        }
+
+    let TouchList = 
+        Class "TouchList"
+
+    let Touch = 
+        Class "Touch" 
+        |=> Nested [TouchInit]
+        |+> Static [
+            Constructor TouchInit?options
+        ]
+        |+> Instance [
+            "identifier" =? T<int>
+            "screenX" =? T<float>
+            "screenY" =? T<float>
+            "clientX" =? T<float>
+            "clientY" =? T<float>
+            "pageX" =? T<float>
+            "pageY" =? T<float>
+            "target" =? T<Dom.Element>
+            "radiusX" =? T<float>
+            "radiusY" =? T<float>
+            "rotationAngle" =? T<float>
+            "force" =? T<float>
+        ]
+
+    TouchList
+    |+> Instance [
+        "length" =? T<int> 
+        "item" => T<int>?index ^-> Touch
+    ] |> ignore
+
+
+    let TouchEventOptions =
+        Pattern.Config "TouchEventOptions" {
+            Required = []
+            Optional = [
+                "touches", TouchList.Type
+                "targetTouches", TouchList.Type
+                "changedTouches", TouchList.Type
+                "ctrlKey", T<bool>
+                "shiftKey", T<bool>
+                "altKey", T<bool>
+                "metaKey", T<bool>
+            ]
+        }
+
+    let TouchEvent =
+        Class "TouchEvent"
+        |=> Inherits T<Dom.Event> 
+        |=> Nested [TouchEventOptions]
+        |+> Static [
+            Constructor (T<string>?``type`` * TouchEventOptions?options)
+        ]
+        |+> Static [
+            "altKey" =? T<bool>
+            "changedTouches" =? TouchList
+            "ctrlKey" =? T<bool>
+            "metaKey" =? T<bool>
+            "shiftKey" =? T<bool>
+            "targetTouches" =? TouchList
+            "touches" =? TouchList
+        ]
+
+    let Assembly =
+        Assembly [
+            Namespace "WebSharper.TouchEvents" [
+                Touch
+                TouchEvent
+                TouchList
+            ]
+        ]
+
+[<Sealed>]
+type Extension() =
+    interface IExtension with
+        member ext.Assembly =
+            Definition.Assembly
+
+[<assembly: Extension(typeof<Extension>)>]
+do ()
